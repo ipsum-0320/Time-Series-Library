aef5e800e8781c78288f98cea30db53aeba5f056
wangxinghao <wangxinghao@xiaohongshu.com>
2024-12-05 21:39:02 +0800

    feat: add new data

---
+++ scripts/long_term_forecast/alibaba/DLinear_alibaba.sh
@@ -0,0 +1,26 @@
+export CUDA_VISIBLE_DEVICES=0,1 # 这表示编号为 0 和编号为 1 的 GPU 是可见的。
+
+model_name=DLinear
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/alibaba/ \
+  --data_path alibaba.csv \
+  --model_id alibaba_96_96 \
+  --model $model_name \
+  --data custom \
+  --features S \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 1 \
+  --dec_in 1 \
+  --c_out 1 \
+  --des 'Exp' \
+  --use_multi_gpu \
+  --use_gpu True \
+  --itr 1
